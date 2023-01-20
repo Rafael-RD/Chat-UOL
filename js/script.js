8644c002e219a4669990cd3624389350c283e278
Rafael <rafaelreinertd@gmail.com>
2023-01-20 03:59:54 -0300

    Recebe Chat

--- js/script.js
+++ js/script.js
@@ -1,36 +1,82 @@
 let intervalId;
+let user={name: ''};
+const principal=document.querySelector('main').querySelector('ul');
 
-function depois(resp){
-    console.log('deu bom');
-    console.log(resp.data);
+function mostrarMensagens(msg){
+    principal.innerHTML='';
+    for(let i=0;i<msg.length-1;i++){
+        const tipo=msg[i].type;
+        if(tipo==='status'){
+            principal.innerHTML+=`
+            <li class="${msg[i].type}"><span>(${msg[i].time}) </span> <span> ${msg[i].from} </span> ${msg[i].text}</li>
+            `
+        }else if(tipo==='message'){
+            principal.innerHTML+=`
+            <li class="${msg[i].type}"><span>(${msg[i].time}) </span> <span> ${msg[i].from} </span> para <span> ${msg[i].to} </span>:  ${msg[i].text}</li>
+            `
+        }else if(tipo==='private_message' && msg[i].to===user.name){
+            principal.innerHTML+=`
+            <li class="${msg[i].type}"><span>(${msg[i].time}) </span> <span> ${msg[i].from} </span> reservadamente para <span> ${msg[i].to} </span>: ${msg[i].text}</li>
+            `
+        }
+        principal.querySelectorAll('li')[principal.querySelectorAll('li').length-1].scrollIntoView();
+    } 
 }
-
-function pegar(resp){
-    console.log('deu ruim');
-    console.log(resp.response.status);
-    if(resp.response.status===400){
-        entrarNaSala();
-    }
-}
-
-function manterLogIn(user){
+function atualizar(){
     const promessaManterLogIn=axios.post('https://mock-api.driven.com.br/api/v6/uol/status',user);
     promessaManterLogIn.then(resp=>{
         console.log('Sessão revalidada');
-        console.log(resp.data);
     });
-    promessaManterLogIn.catch()
-
+    promessaManterLogIn.catch(resp=>{
+        alert('Revalidação falhada');
+        console.log(resp.response.status);
+    })
+    const chat=axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    chat.then(resp=>{
+        console.log('chat recebido');
+        mostrarMensagens(resp.data);
+    });
+    chat.catch(resp=>{
+        console.log('erro ao receber msgs '+resp.response.status);
+        console.log(resp.response);
+    })
 }
 
-function entrarNaSala(nome){
-    const user={name: nome};
+function entrarNaSala(){
+    user.name=prompt('nome: ');
     const promessaLogIn=axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',user);
     console.log(promessaLogIn);
-    promessaLogIn.then(depois);
-    promessaLogIn.catch(pegar);
-    intervalId=setInterval(manterLogIn, 4000, user);
+    promessaLogIn.then(resp=>{    
+        console.log('Login Feito');
+        console.log(resp.data);
+        intervalId=setInterval(atualizar, 3000, user);
+    });
+    promessaLogIn.catch(resp=>{
+        console.log('Erro login');
+        console.log(resp.response.status);
+        if(resp.response.status===400){
+            alert('Usuario ja existe!');
+            entrarNaSala();
+        }
+    });
 }
 
-let nome=prompt('Nome:');
-if(nome!==null) entrarNaSala(nome);
+
+function abc(){
+    for(let i=0;i<arguments.length;i++){
+        console.log(arguments[i]);
+    }
+}
+
+
+
+entrarNaSala();
+
+
+// user.name=prompt('Nome:');
+// if(user.name!==null) entrarNaSala();
+// const msgs=axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+//  msgs.then(resp=>{
+//      console.log(resp);
+//      mostrarMensagens(resp.data);
+//  });
